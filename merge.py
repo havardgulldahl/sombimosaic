288ec2374527256920220d3d5eb785d2fea1f007
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2015-10-14 02:10:52 +0200

    make it work

--- merge.py
+++ merge.py
@@ -5,8 +5,13 @@ from wand.image import Image
 
 
 if __name__ == '__main__':
+  # just run
+  # montage sombies/*.png sombies/poster2.pdf
+
+  """
   import sys
   with Image(sys.argv[1]) as original:
     with original.convert('png') as converted:
       converted.trim(color=None, fuzz=0.33)
       converted.save(filename='trimd.png')
+  """
--- pull.py
+++ pull.py
@@ -3,26 +3,27 @@
 
 import requests, logging, json, os.path
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 SOMBIURL='http://sombi.nrk.no/api/1.2/data/?limit={limit}&skip={skip}&moderation=1&metadataQuery=true&project_id=552e77b7fd3cf1c636eda6d7'
-SKIPSTEP=50
+LIMITREQUESTS=500
 
-def sombiiter():
+def sombiiter(session):
   c = 1
-  start = requests.get(SOMBIURL.format(limit=c, skip=0)) # get first record
+  start = session.get(SOMBIURL.format(limit=1, skip=0)) # get first record
   totalrecords = start.json()['metadata']['count']
-  logging.debug('Total %s records', totalrecords)
+  logging.info('Total %s records', totalrecords)
   while c < totalrecords:
-    logging.debug('Gettting SOMBIes from #%s, skipping %s', c, SKIPSTEP)
-    r = requests.get(SOMBIURL.format(limit=c, skip=SKIPSTEP))
+    logging.info('Gettting %s SOMBIes, skipping %s', LIMITREQUESTS, c)
+    r = session.get(SOMBIURL.format(limit=LIMITREQUESTS, skip=c))
     for record in r.json()['results']:
       yield record
-    c = c + SKIPSTEP
+    c = c + LIMITREQUESTS
 
 if __name__ == '__main__':
   folder = './sombies'
-  for sombi in sombiiter():
+  sess = requests.Session()
+  for sombi in sombiiter(session=sess):
     logging.debug('Got new sombie: %s', sombi['id'])
     sidecar = os.path.join(folder, sombi['id'] + '.json')
     pngfile = os.path.join(folder, sombi['id'] + '.png')
@@ -31,7 +32,7 @@ if __name__ == '__main__':
       logging.debug('Wrote JSON: %s', sombi['title'])
     if not os.path.exists(pngfile):
       with open(pngfile, 'wb') as pf:
-        pf.write(requests.get(sombi['image']['standard']).read())
+        pf.write(sess.get(sombi['image']['standard']).content)
         logging.debug('Wrote PNG: %s', sombi['title'])
 
 
